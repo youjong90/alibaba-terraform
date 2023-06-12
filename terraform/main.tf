77d7b8a1bdddf9ee0e62b94966344833bc7b6e7a
youjong90 <kiv757@naver.com>
2023-06-12 13:56:32 +0800

    cache clear

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "alicloud" {
-	access_key = var.alicloud_access_key
-	secret_key = var.alicloud_secret_key
+	access_key = var.alicloud_access_key != "" ? var.alicloud_access_key : var.env.ALICLOUD_ACCESS_KEY
+	secret_key = var.alicloud_secret_key != "" ? var.alicloud_secret_key : var.env.ALICLOUD_SECRET_KEY
+	region = "ap-southeast-1"
 }
-
